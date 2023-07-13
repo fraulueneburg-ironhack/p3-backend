66056bc95bb4d1a353527c9899385532db79254e
Wiebke Lueneburg <git@fraulueneburg.de>
2023-07-13 18:24:35 +0200

    added daily expense routes

--- routes/budget.routes.js
+++ routes/budget.routes.js
@@ -1,63 +1,93 @@
 const MonthlyBudget = require('../models/MonthlyBudget.model')
-const UserModel = require('../models/User.model')
+const DailyExpenses = require('../models/DailyExpenses.model')
 const router = require('express').Router()
 const { isAuthenticated } = require('../middlewares/jwt.auth')
 
 // BUDGET ROUTE
 
-router.get('/:budgetId', async (req, res) => {
-  const allBudgets = await MonthlyBudget.findById(req.params.budgetId)
-  res.json(allBudgets)
+router.get('/', isAuthenticated, async (req, res) => {
+	const userId = req.payload._id
+	const foundMonthlyBudget = await MonthlyBudget.find({ user: userId })
+	const foundDailyExpenses = await DailyExpenses.find({ user: userId })
+	res.json({ respMonthlyBudget: foundMonthlyBudget, respDailyExpenses: foundDailyExpenses })
 })
 
 // CREATE/UPDATE BUDGET ROUTE
 
 router.post('/create', isAuthenticated, async (req, res) => {
-  try {
-    const userId = req.payload._id
-    const newBudgetData = req.body
-    const budget = await MonthlyBudget.find({ user: userId })
+	try {
+		const userId = req.payload._id
+		const newBudgetData = req.body
+		const budget = await MonthlyBudget.find({ user: userId })
 
-    console.log("REQ.BODY", req.body);
-    console.log("NEW BUDGET DATA", newBudgetData);
-
-    if (budget === []) {
-      const newBudget = await MonthlyBudget.create({
-        user: userId,
-        currency: newBudgetData.currency,
-        earnings: newBudgetData.earnings,
-        expenses: newBudgetData.expenses,
-        spendingCategories: newBudgetData.spendingCategories,
-      })
-      res.status(201).json(newBudget)
-    } else {
-      const updatedBudget = await MonthlyBudget.findByIdAndUpdate(
-        budget[0]._id,
-        {
-          currency: newBudgetData.currency,
-          earnings: newBudgetData.earnings,
-          expenses: newBudgetData.expenses,
-          spendingCategories: newBudgetData.spendingCategories,
-        },
-        { new: true }
-      )
-      console.log("UPDATED BUDGET", newBudgetData);
-      res.status(201).json(updatedBudget)
-    }
-  } catch (err) {
-    console.log(err)
-  }
+		if (budget === []) {
+			const newBudget = await MonthlyBudget.create({
+				user: userId,
+				currency: newBudgetData.currency,
+				earnings: newBudgetData.earnings,
+				expenses: newBudgetData.expenses,
+				spendingCategories: newBudgetData.spendingCategories,
+			})
+			res.status(201).json(newBudget)
+		} else {
+			const updatedBudget = await MonthlyBudget.findByIdAndUpdate(
+				budget[0]._id,
+				{
+					currency: newBudgetData.currency,
+					earnings: newBudgetData.earnings,
+					expenses: newBudgetData.expenses,
+					spendingCategories: newBudgetData.spendingCategories,
+				},
+				{ new: true }
+			)
+			res.status(201).json(updatedBudget)
+		}
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 // DELETE BUDGET ROUTE
 
 router.post('/:budgetId/delete', async (req, res) => {
-  try {
-    const deletedBudget = await MonthlyBudget.findByIdAndDelete({ _id: req.params.budgetId })
-    res.redirect(`/budget`)
-  } catch (err) {
-    console.log(err)
-  }
+	try {
+		const deletedBudget = await MonthlyBudget.findByIdAndDelete({ _id: req.params.budgetId })
+		res.redirect(`/budget`)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+// ADD DAILY EXPENSE ROUTE
+
+router.post('/addexpense', isAuthenticated, async (req, res) => {
+	try {
+		const userId = req.payload._id
+		const dailyExpenseData = req.body
+		console.log('dailyExpenseData', dailyExpenseData)
+		const newDailyExpense = await DailyExpenses.create({
+			date: dailyExpenseData.date,
+			user: userId,
+			category: dailyExpenseData.category,
+			name: dailyExpenseData.name,
+			amount: dailyExpenseData.amount,
+		})
+		console.log('newDailyExpense', newDailyExpense)
+		res.status(201).json(newDailyExpense)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+// DELETE DAILY EXPENSE ROUTE
+
+router.post('/:dailyExpenseId/delete', async (req, res) => {
+	try {
+		const deletedBudget = await DailyExpenses.findByIdAndDelete({ _id: req.params.dailyExpenseId })
+		res.redirect(`/budget`)
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 module.exports = router
